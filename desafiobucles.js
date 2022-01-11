6f16df332b6f7fd158479fc87ddef7a5bd7a1d41
Posheyla <luisa.virgo.98@hotmail.com>
2022-01-10 21:27:39 -0500

    Segundo ejercicio JS

---
+++ desafiobucles.js
@@ -0,0 +1,28 @@
+
+for(let i=1; i<21; i++){
+    if ( i % 2 != 0) {
+        console.log(i);
+    }
+}
+
+for (let j=100;j>=0;j--){
+    if(j%3==0){
+        console.log(j);
+    }
+}
+
+for (let p=4;p >= -3.5; p-=1.5){
+    console.log(p);
+}
+
+let sum=0;
+for ( a=1; a<101;a++){
+    sum=sum+a;
+}
+console.log(sum);
+
+let product=1;
+for ( b=1; b<13;b++){
+    product=product*b;
+}
+console.log(product);
